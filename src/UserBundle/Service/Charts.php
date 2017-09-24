da27783bf2b60457f0d8626638e37fa4201d4f30
yauheni_novikau <e4.novikov@itransition.com>
2017-09-24 17:43:58 +0300

    add ingredients count

--- src/UserBundle/Service/Charts.php
+++ src/UserBundle/Service/Charts.php
@@ -4,36 +4,8 @@ namespace UserBundle\Service;
 class Charts
 {
     /**
-     * Calculate ingredients cost
-     * @data array
-     * @beenCost integer
-     * $amortization integer
-     * @return integer
-     */
-    public function calculateCost($data, $beenCost, $amortization)
-    {
-        $user = $data['user'];
-        $userType = $user->getType();
-        $ingredients = $data['ingredients'];
-        $ingredientsCost = 0;
-        $beenCost = $data['choice'] * $beenCost;
-
-        foreach ($ingredients as $ingredient) {
-            $ingredientsCost = $ingredientsCost + $ingredient->getCost();
-        }
-
-        $cost = $beenCost + $ingredientsCost;
-
-        if ($userType == 'buyer') {
-            $cost = $cost + $amortization;
-        }
-
-        return $cost;
-    }
-
-    /**
      * Structure chart data
-     * @userCups array
+     * @param array $userCups
      * @return array
      */
     public function prepareChartData($userCups)
